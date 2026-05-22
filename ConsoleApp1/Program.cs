d269ef943f6c02bd6a01fd7508a612342f462cbd
GiorgiPatchkoria <giorgi.patchkoria.1@btu.edu.ge>
2026-05-22 13:10:51 +0400

    davaleba 5

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,68 +5,25 @@
         static void Main(string[] args) {
 
             #region Davaleba N1
+            int[] array1 = [1,2,3];
+            int[] array2 = [4,5,6];
 
-            Console.WriteLine("შეიყვანეთ რიცხვი:");
-            int number1;
-            bool numberIsGood1 = int.TryParse(Console.ReadLine(), out number1);
-            int result1;
+            int[] resultArray = new int[array1.Length + array2.Length];
 
-            if (numberIsGood1) {
-                for (int i = 1; i <= 10; i++) {
-                    result1 = number1 * i;
-                    Console.WriteLine($"{number1} * {i} = {result1}");
-                }
-            } else {
-                Console.WriteLine("Incorrect Number");
+            for (int i = 0; i< array1.Length; i++)
+            {
+                resultArray[i] = array1[i];
             }
 
-            #endregion
-
-            #region Davaleba N2
-
-            for (int i = 1; i <= 4; i++)  {
-                for (int j = 1; j <= 4-i; j++) {
-                    Console.Write(" ");
-                } 
-                for (int h = 1; h <= i; h++) {
-                    Console.Write("* ");
-                }
-                Console.WriteLine();
+            for (int i = 0;i< array2.Length; i++)
+            {
+                resultArray[array1.Length + i] = array2[i];
             }
 
-            #endregion
-
-            #region Davaleba N3
-
-            Console.WriteLine("შეიყვანეთ რიცხვი:");
-            int number3;
-            bool numberIsGood3 = int.TryParse(Console.ReadLine(), out number3);
-            int result3 = 0;
-
-            if (numberIsGood3) {
-                for (int i = 1; i < number3; i++)  {
-                    if (i % 2 == 0) {
-                        result3 += i;
-                    }
-                }
-                Console.WriteLine($"ჯამი არის {result3}");
-            } else {
-                Console.WriteLine("Incorrect Number");
+            foreach (int i in resultArray)
+            {
+                Console.WriteLine(i);
             }
-
-            #endregion
-
-            #region Davaleba N4
-
-            Random random = new Random();
-            int randomNumber = random.Next(0, 10);
-            int number4;
-
-            do {
-                Console.WriteLine("შეიყვანეთ რიცხვი:");
-                bool numberIsGood4 = int.TryParse(Console.ReadLine(), out number4);
-            } while (number4 != randomNumber);
-
             #endregion
         }
     }
