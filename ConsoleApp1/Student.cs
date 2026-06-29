6f84975a802d3d211963a9cbb0027054bc89a609
GiorgiPatchkoria <giorgi.patchkoria.1@btu.edu.ge>
2026-06-30 00:04:47 +0400

    16-Delegates and algorithms

---
+++ ConsoleApp1/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp1
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public double GPA { get; set; }
+
+        public Student(string name, double gpa)
+        {
+            Name = name;
+            GPA = gpa;
+        }
+    }
+}
